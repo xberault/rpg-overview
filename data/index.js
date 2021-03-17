297564c3a1df4cc359ab2227321e7c49a335a8ff
Xavier Berault <xavier.berault@etu.univ-orleans.fr>
2021-03-17 09:39:07 +0100

    adding json-serve via npm run serve

---
+++ data/index.js
@@ -0,0 +1,11 @@
+const classes = require('./class.json');
+const items = require('./items.json');
+const players = require('./player.json')
+// Something more
+
+module.exports = () => ({
+
+  classes: classes,
+  items: items,
+  players : players
+});
