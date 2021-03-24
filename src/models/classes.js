eb8c19857817e21d4a6cca0b2b9cf4e8bfb072a0
Amki45 <amkidelanneau@gmail.com>
2021-03-24 09:39:44 +0100

    Classes.js class on left_nav

--- src/models/classes.js
+++ src/models/classes.js
@@ -2,8 +2,34 @@
 import { HTMLHandler } from "./htmlhandler";
 export class Classes extends HTMLHandler{
 
-    static goTo(){
-        HTMLHandler.leftNav("characters.html");
+    static getAllClasses(){
+        $.ajax({
+            url: "http://localhost:3000/classes",
+            type: "GET",
+            dataType: "json",
+            success: function(classes) {
+                classes = classes.class // plus simple et compréhensible
+                console.log(JSON.stringify(classes));
+                for(let i=0; i<classes.length;i++){
+                    console.log("Bienvenue");
+                    $('#classes')
+                    .append($('<li>')
+                    .append($('<a>')
+                        .text(classes[i].title)
+                        )
+                    );
+                }
+            },
+            error: function(req, status, err) {
+                $("#classes").html("<b>Impossible de récupérer les classes !</b>");
+            }
+        })
+
+
     }
 
+    static goTo(){
+        Classes.getAllClasses();
+        HTMLHandler.leftNav("classes.html");
+    }
 }
