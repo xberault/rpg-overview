027926af37366129b993a39b0ff2daca6302a5fa
berault_xavier <xavier.berault@gmail.com>
2021-03-28 15:11:58 +0200

    added css + beautify index

--- src/index.js
+++ src/index.js
@@ -1,35 +1,37 @@
-import { Character } from "./models/characters.js";
-import { HTMLHandler } from "./models/htmlhandler.js";
-import { Classes } from "./models/classes.js";
+import {Character} from "./models/characters.js";
+import {HTMLHandler} from "./models/htmlhandler.js";
+import {Classes} from "./models/classes.js";
+import './styles/main.css';
 
-$(window).on('load hashchange', function(e) {
+
+$(window).on('load hashchange', function (e) {
     console.log("conneted")
     try {
-      SPARouter_hash(window.location.hash);
+        SPARouter_hash(window.location.hash);
     } catch (err) {
-      console.error(err)
+        console.error(err)
 
     }
-  });
+});
 
 
 function SPARouter_hash(page) {
-    switch(page) {
-      case '':
-      case '#':
-      case '#home':
-          HTMLHandler.nav('index.html');
-          Character.goTo();
-        break;
-      case '#characters':
-          Character.goTo();
-        break;
-      case '#classes':
-        Classes.goTo();
-        break;
-      default:
-        HTMLHandler.Nav('404.html');
+    switch (page) {
+        case '':
+        case '#':
+        case '#home':
+            HTMLHandler.nav('index.html');
+            Character.goTo();
+            break;
+        case '#characters':
+            Character.goTo();
+            break;
+        case '#classes':
+            Classes.goTo();
+            break;
+        default:
+            HTMLHandler.Nav('404.html');
     }
-  }
+}
 
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,23 @@
 const path = require('path');
 module.exports = {
- "mode": "none",
- "entry": "./src/index.js",
- "output": {
-   "path": __dirname + '/dist',
-   "filename": "bundle.js"
- },
-devServer: {
-   contentBase: path.join(__dirname, 'dist')
- }
+    "mode": "none",
+    "entry": "./src/index.js",
+    "output": {
+        "path": __dirname + '/dist',
+        "filename": "bundle.js"
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist')
+    },
+    "module": {
+        "rules": [
+            {
+                "test": /\.css$/,
+                "use": [
+                    "style-loader",
+                    "css-loader"
+                ]
+            },
+        ]
+    }
 }
